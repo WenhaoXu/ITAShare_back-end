649ece8ea7f59c6bb65b9da04fdcae910747e319
weidy2 <dylan.y.q.wei@oocl.com>
2019-03-24 11:40:58 +0800

    updated the port

--- src/main/java/com/oocl/demo/TodoListApplication.java
+++ src/main/java/com/oocl/demo/TodoListApplication.java
@@ -1,16 +1,14 @@
 package com.oocl.demo;
 
-import com.oocl.demo.repository.TodoListRepositpory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DemoApplication {
+public class TodoListApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(TodoListApplication.class, args);
     }
 
 }
